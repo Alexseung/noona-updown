85d6ab75bb3d293b7f7ef8bea164e9d96bf3aed4
Seunggeun Yoon <tmdmrs522@gmail.com>
2024-06-30 22:34:00 +0900

    last

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ inputArea.addEventListener('focus', () => {
 });
 function randomNum() {
   computerNum = Math.floor(Math.random() * 100 + 1);
-  // console.log(computerNum);
+  console.log(computerNum);
 }
 
 //enter키 입력 무조건...없으면 불편함
@@ -48,6 +48,7 @@ function reset() {
   //img를 원래 이미지로 다시 설정해주어야하기에 img다시 설정
   img.src =
     'https://ojsfile.ohmynews.com/CRI_T_IMG/2018/0813/A0002462761_T.jpg';
+  inputArea.addEventListener('keydown', handleKeyDown);
 }
 
 function play() {
@@ -73,6 +74,10 @@ function play() {
     //숫가자 맞으면 img를 변경해주어야 하기때문에 img도 변수선언하여 이렇게 가져오기
     img.src =
       'https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/7YmD/image/rfz9SSvcbysKDLNtM9ZkR2C0_m4.jpg';
+    imgDesign.style.opacity = 0.8;
+    inputArea.removeEventListener('keydown', handleKeyDown);
+    playBtn.disabled = true;
+
     return;
   }
 
@@ -82,14 +87,27 @@ function play() {
   //불투명도값을 올린다
   imgDesign.style.opacity = parseFloat(imgDesign.style.opacity) + 0.16;
 
-  if (chances < 1) {
+  //정답을 맞추지 못 하였을 경우 정답을 표시해주고싶었으나 chances가 0이면 그냥 실패라고 인식하여 alert가 나오기에 코드바꿈
+  if (chances < 1 && inputValue != computerNum) {
     gameOver = true;
   }
 
   if (gameOver == true) {
     playBtn.disabled = true;
-    result.textContent = '끝났습니다...';
+    result.textContent = '또 하쉴??';
+    if (inputValue != computerNum) {
+      alert(`아쉽네!!! ${computerNum}인데!`);
+    }
   }
 }
+
+// if (chances < 1) {
+//   gameOver = true;
+// }
+
+// if (gameOver == true) {
+//   playBtn.disabled = true;
+//   result.textContent = '끝났습니다...';
+// }
 
 randomNum();
